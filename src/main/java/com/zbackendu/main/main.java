5a019458d874e49c6a8366468d0d72aaccaabd1c
Parodix007 <sebastian.siarczynski@gmail.com>
2022-03-24 20:47:44 +0100

    listy i tablice

--- src/main/java/com/zbackendu/main/main.java
+++ src/main/java/com/zbackendu/main/main.java
@@ -1,7 +1,33 @@
 package com.zbackendu.main;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("HELLO SEBASTIAN");
+        List<Integer> input = new ArrayList<>(5);
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            System.out.print("PODAJ LICZBE: ");
+            int i1 = scanner.nextInt();
+            input.add(i1);
+        }
+        int najwieksza = 0;
+        for (Integer element : input) {
+            if (element > najwieksza) najwieksza = element;
+        }
+        System.out.println(najwieksza);
+
+        int i = 0;
+        while (i < 5) {
+            System.out.println("Hello world");
+            i = 5;
+        }
+
+        do {
+            System.out.println("Hello world");
+            i = 5;
+        } while (i < 5);
     }
 }
