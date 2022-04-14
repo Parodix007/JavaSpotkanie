a5e1ddd2d1703be2406767f84272d7029cc937ae
Parodix007 <sebastian.siarczynski@gmail.com>
2022-04-14 18:42:34 +0200

    finish

--- src/main/java/matura2021/Matura2021.java
+++ src/main/java/matura2021/Matura2021.java
@@ -6,7 +6,9 @@ import matura2021.models.Tasks;
 
 public class Matura2021 {
     public static void main(String[] args) {
-        Tasks tasks = Tasks.tasksFrom(new FileReaderImpl(), new ParserImpl());
-        tasks.test();
+        Tasks tasks = Tasks.tasksFrom(new FileReaderImpl(), new ParserImpl(), "matura2021/galerie_przyklad.txt");
+        tasks.zadanie1();
+        tasks.zadani2();
+        tasks.zadani3();
     }
 }
